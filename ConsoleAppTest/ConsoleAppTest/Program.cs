a11edb0156eb9812e3392eb87f8c50d5fac12891
Tim-Dr-T-Chamillard <Tim-Dr-T-Chamillard@users.noreply.github.com>
2016-12-24 07:09:52 -0700

    Initial commit

---
+++ ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleAppTest {
+
+    /// <summary>
+    /// Tests a console app with GitHub
+    /// </summary>
+    class Program {
+
+        /// <summary>
+        /// Tests a console app with GitHub
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args) {
+        }
+    }
+}
